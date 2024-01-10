7f64af44aec1a1083d60d317be99a90dd5dabb71
Aleksey525 <alex_tolchin@mail.ru>
2024-01-11 00:51:09 +0300

    Создал репозиторий

---
+++ app.py
@@ -0,0 +1,17 @@
+import requests
+
+def get(u: str, p: dict) -> str:
+    response = requests.get(u, params=p)
+    response.raise_for_status()
+    return response.text
+
+
+params = {'lang': 'ru', 'T': '', 'm': '', 'M': '', 'n': '', 'q': ''}
+cities = ['Лондон', 'Шереметьево', 'Череповец']
+url_template = 'https://wttr.in/{}?'
+
+
+if __name__ == '__main__':
+    for i in cities:
+        url = url_template.format(i)
+        print(get(url, params))
