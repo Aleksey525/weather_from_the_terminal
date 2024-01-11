d56c86661014af165cca8f6e95157df29b5989a8
Aleksey525 <alex_tolchin@mail.ru>
2024-01-11 12:44:14 +0300

    changed variable names

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ cities = ['Лондон', 'Шереметьево', 'Череповец']
 url_template = 'https://wttr.in/{}?'
 
 
-def get(u: str, p: dict) -> str:
-    response = requests.get(u, params=p)
+def get_response_text(url_complite: str, user_params: dict) -> str:
+    response = requests.get(url_complite, params=user_params)
     response.raise_for_status()
     return response.text
 
 
 if __name__ == '__main__':
-    for i in cities:
-        url = url_template.format(i)
-        print(get(url, params))
+    for city in cities:
+        url = url_template.format(city)
+        print(get_response_text(url, params))
