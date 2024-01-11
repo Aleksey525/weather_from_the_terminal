a20059d59cf03bd8675d55aee8e03cb2767330a2
Aleksey525 <alex_tolchin@mail.ru>
2024-01-11 09:36:07 +0300

    upgrade add.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import requests
 
-def get(u: str, p: dict) -> str:
-    response = requests.get(u, params=p)
-    response.raise_for_status()
-    return response.text
-
 
 params = {'lang': 'ru', 'T': '', 'm': '', 'M': '', 'n': '', 'q': ''}
 cities = ['Лондон', 'Шереметьево', 'Череповец']
 url_template = 'https://wttr.in/{}?'
+
+def get(u: str, p: dict) -> str:
+    response = requests.get(u, params=p)
+    response.raise_for_status()
+    return response.text
 
 
 if __name__ == '__main__':
