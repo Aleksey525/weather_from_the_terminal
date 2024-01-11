58940698673b0767e337eb5b2ed68a367b5ff963
Aleksey525 <alex_tolchin@mail.ru>
2024-01-11 09:43:05 +0300

    upgrade app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ params = {'lang': 'ru', 'T': '', 'm': '', 'M': '', 'n': '', 'q': ''}
 cities = ['Лондон', 'Шереметьево', 'Череповец']
 url_template = 'https://wttr.in/{}?'
 
+
 def get(u: str, p: dict) -> str:
     response = requests.get(u, params=p)
     response.raise_for_status()
