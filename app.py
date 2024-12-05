217eb4f22b0400e3e56250fab16e413572d16e09
Aleksey525 <alex_tolchin@mail.ru>
2024-12-05 19:18:57 +0300

    Formated the code

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import requests
 
 
-params = {'lang': 'ru', 'T': '', 'm': '', 'M': '', 'n': '', 'q': ''}
-cities = ['Лондон', 'Шереметьево', 'Череповец']
-url_template = 'https://wttr.in/{}?'
-
-
 def get_response_text(url_complite: str, user_params: dict) -> str:
     response = requests.get(url_complite, params=user_params)
     response.raise_for_status()
     return response.text
 
 
-if __name__ == '__main__':
+def main():
+    params = {'lang': 'ru', 'T': '', 'm': '', 'M': '', 'n': '', 'q': ''}
+    cities = ['Лондон', 'Шереметьево', 'Череповец']
+    url_template = 'https://wttr.in/{}?'
     for city in cities:
         url = url_template.format(city)
         print(get_response_text(url, params))
+
+
+if __name__ == '__main__':
+    main()
